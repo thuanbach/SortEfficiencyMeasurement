f4b532cce3850a9a61fb4ea47130f5517b9a95d0
Thuan Bach <ThuanBach@uregina.ca>
2021-10-10 12:05:51 +0700

    Add "Path coverage" for the cos and sin  method

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,113 @@
 //============================================================================
 
 #include <iostream>
+
+
 using namespace std;
 
+int sin(int x);
+int cos(int x);
+
+
+void verifySinMethod(int degree, int expected){
+	int actual = sin(degree);
+
+	cout << "sin(" <<  degree  << ")" << " is " << actual <<", expected " << expected << ": ";
+
+	if (actual == expected) {
+		cout << "Pass";
+	} else {
+		cout << "Fail";
+	}
+
+	cout << endl;
+}
+
+
+void verifyCosMethod(int degree, int expected){
+	int actual = sin(degree);
+
+	cout << "cos(" <<  degree  << ")" << " is " << actual <<", expected " << expected << ": ";
+
+	if (actual == expected) {
+		cout << "Pass";
+	} else {
+		cout << "Fail";
+	}
+
+	cout << endl;
+}
+
+void ruTestSuiteForSinMethod() {
+	cout << "******Start running test cases for sin method*****" << endl;
+
+	verifySinMethod(-30, 4999);
+
+	verifySinMethod(-60, 8660);
+
+	verifySinMethod(-120, 8660);
+
+	verifySinMethod(-160, 3419);
+
+	verifySinMethod(-210, -4999);
+
+	verifySinMethod(-240, -8660);
+
+	verifySinMethod(30, 4999);
+
+	verifySinMethod(60, 8660);
+
+	verifySinMethod(120, 8660);
+
+	verifySinMethod(160, 3419);
+
+	verifySinMethod(210, -4999);
+
+	verifySinMethod(240, -8660);
+
+	cout << "*****End test suite sin method****" << endl;
+
+}
+
+
+void ruTestSuiteForCosMethod() {
+	cout << "******Start running test cases for cos method*****" << endl;
+	verifyCosMethod(-30, 4999);
+
+	verifyCosMethod(-60, 8660);
+
+	verifyCosMethod(-120, 8660);
+
+	verifyCosMethod(-160, 3419);
+
+	verifyCosMethod(-210, -4999);
+
+	verifyCosMethod(-240, -8660);
+
+
+	verifyCosMethod(30, 4999);
+
+	verifyCosMethod(60, 8660);
+
+	verifyCosMethod(120, 8660);
+
+	verifyCosMethod(160, 3419);
+
+	verifyCosMethod(210, -4999);
+
+	verifyCosMethod(240, -8660);
+
+	cout << "*****End test suite cos method****" << endl;
+}
+
+
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	ruTestSuiteForSinMethod();
+
+	ruTestSuiteForCosMethod();
+
+
 	return 0;
 }
