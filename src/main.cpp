3e7f5e29d557a8a63e1bfdcc2966f99f879f58f4
Thuan Bach <ThuanBach@uregina.ca>
2021-10-10 15:30:15 +0700

    Test boundary conditions for sin methods and correct PATH coverage tests for cos method

--- src/main.cpp
+++ src/main.cpp
@@ -30,22 +30,12 @@ void verifySinMethod(int degree, int expected){
 }
 
 
-void verifyCosMethod(int degree, int expected){
-	int actual = sin(degree);
 
-	cout << "cos(" <<  degree  << ")" << " is " << actual <<", expected " << expected << ": ";
 
-	if (actual == expected) {
-		cout << "Pass";
-	} else {
-		cout << "Fail";
-	}
-
-	cout << endl;
-}
-
-void ruTestSuiteForSinMethod() {
+void runTestSuiteForSinMethod() {
 	cout << "******Start running test cases for sin method*****" << endl;
+
+	cout << "1. **Test PATH Coverage**" << endl;
 
 	verifySinMethod(-30, 4999);
 
@@ -71,37 +61,80 @@ void ruTestSuiteForSinMethod() {
 
 	verifySinMethod(240, -8660);
 
+
+
+	// Test Boundaries
+	cout << "2. **Test Boundaries**" << endl;
+
+	verifySinMethod(0, 0);
+
+	verifySinMethod(360, 0);
+
+	verifySinMethod(45, 7071);
+
+	verifySinMethod(-45, 7071);
+
+	verifySinMethod(-90, 9999);
+
+	verifySinMethod(90, 9999);
+
+	verifySinMethod(180, 0);
+
+	verifySinMethod(-180, 0);
+
+
+	verifySinMethod(-405, 7071);
+	verifySinMethod(405, 7071);
+
+	verifySinMethod(-450, 9999);
+	verifySinMethod(450, 9999);
+
+	verifySinMethod(-540, 0);
+	verifySinMethod(540, 0);
+
+	verifySinMethod(-720, 0);
+	verifySinMethod(720, 0);
+
+
 	cout << "*****End test suite sin method****" << endl;
+	cout << endl;
 
 }
 
 
-void ruTestSuiteForCosMethod() {
-	cout << "******Start running test cases for cos method*****" << endl;
-	verifyCosMethod(-30, 4999);
+void verifyCosMethod(int degree, int expected){
+	int actual = sin(degree);
 
-	verifyCosMethod(-60, 8660);
+	cout << "cos(" <<  degree  << ")" << " is " << actual <<", expected " << expected << ": ";
+
+	if (actual == expected) {
+		cout << "Pass";
+	} else {
+		cout << "Fail";
+	}
+
+	cout << endl;
+}
+
+
+void runTestSuiteForCosMethod() {
+	cout << "******Start running test cases for cos method*****" << endl;
+
+	// Test Path Coverage
+	cout << "1. Test PATH Coverage" << endl;
 
 	verifyCosMethod(-120, 8660);
-
-	verifyCosMethod(-160, 3419);
-
+	verifyCosMethod(-150, 4999);
 	verifyCosMethod(-210, -4999);
-
-	verifyCosMethod(-240, -8660);
-
-
+	verifyCosMethod(-250, -9395);
+	verifyCosMethod(-300, -8660);
+	verifyCosMethod(-330, -4999);
+	verifyCosMethod(-60, 8660);
+	verifyCosMethod(-30, 4999);
 	verifyCosMethod(30, 4999);
-
-	verifyCosMethod(60, 8660);
-
+	verifyCosMethod(70, 9395);
 	verifyCosMethod(120, 8660);
-
-	verifyCosMethod(160, 3419);
-
-	verifyCosMethod(210, -4999);
-
-	verifyCosMethod(240, -8660);
+	verifyCosMethod(150, 4999);
 
 	cout << "*****End test suite cos method****" << endl;
 }
@@ -110,9 +143,9 @@ void ruTestSuiteForCosMethod() {
 
 int main() {
 
-	ruTestSuiteForSinMethod();
+	runTestSuiteForSinMethod();
 
-	ruTestSuiteForCosMethod();
+	runTestSuiteForCosMethod();
 
 
 	return 0;
